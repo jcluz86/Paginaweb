eecb2eedfb20a9420f5fd1b3d2a4e592b6187400
jcluz86 <julio.c.luz22@gmail.com>
2022-12-03 17:28:53 -0300

    commit

---
+++ script/samba.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export PATH="$PATH:/sbin:/usr/sbin:usr/local/sbin"
+cp /etc/samba/smb.conf /etc/samba/smb.conf.bak
+mkdir /media/dados/$1
+chmod 777 -R /media/dados/$1
+echo "usershare path = /var/lib/samba/usershares/" >> /etc/samba/smb.conf
+chgrp informatica /media/dados/$1
+useradd -m $1
+(echo $2; echo $2) | smbpasswd -a $1
+usermod -G informatica $1
+echo "["$1"]" >> /etc/samba/smb.conf
+echo "path = /media/"$1 >> /etc/samba/smb.conf
+echo "valid users = @informatica" >> /etc/samba/smb.conf
+echo "read only = No" >> /etc/samba/smb.conf
+echo "create mask = 0700" >> /etc/samba/smb.conf
+echo "directory mask = 0700" >> /etc/samba/smb.conf
+service smbd restart
+
+
+
